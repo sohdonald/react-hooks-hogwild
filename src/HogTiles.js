5c4b725582cfa9ca7bdb37301f8ad55040827719
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-05 16:58:22 -0400

    working on HogTiles

---
+++ src/HogTiles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HogTiles() {
+    // call hogs here, get hog's name and image displayed
+  // the keys in hogs object array are name: and image:
+    return(
+        <></>
+    );
+}
+
+export default HogTiles;
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
+import HogTiles from "../HogTiles";
 
 import hogs from "../porkers_data";
 
-function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+function App({ hogName, hogImg }) {
+  // call hogs here, get hog's name and image displayed
+  // the keys in hogs object array are name: and image:
+  // create a new component to handle the tiles
+
+  return (
+    <div className="App">
+      <Nav />
+      <HogTiles name={hogName.name} image={hogImg.image} />
+    </div>
+  );
 }
 
 export default App;
